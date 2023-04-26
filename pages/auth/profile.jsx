c607337dcadd87659d675c1457eb61614185edbd
Bienvenu <faraja.bien@gmail.com>
2023-04-26 21:05:03 +0300

    starter profile view

--- pages/auth/profile.jsx
+++ pages/auth/profile.jsx
@@ -4,7 +4,43 @@ import { useDispatch, useSelector } from 'react-redux'
 import { register } from '../../actions/authActions'
 import { getUniversities, getUniversity } from '../../actions/uniActions'
 import Select from 'react-select'
-import { FaStar, FaTrash } from 'react-icons/fa'
+import {
+	FaBook,
+	FaPlusSquare,
+	FaStar,
+	FaTrash,
+	FaUserTie,
+} from 'react-icons/fa'
+
+const watchListLecsSample = [
+	{
+		_id: '1',
+		name: 'John Doe',
+		email: 'johndoe@example.com',
+		university: {
+			code: 'UCLA',
+		},
+		avgRating: 4.8,
+	},
+	{
+		_id: '2',
+		name: 'Jane Smith',
+		email: 'janesmith@example.com',
+		university: {
+			code: 'MIT',
+		},
+		avgRating: 4.3,
+	},
+	{
+		_id: '3',
+		name: 'Bob Johnson',
+		email: 'bobjohnson@example.com',
+		university: {
+			code: 'Stanford',
+		},
+		avgRating: 4.6,
+	},
+]
 
 const StudentProfile = () => {
 	const { user, isLoading } = useUser()
@@ -16,11 +52,12 @@ const StudentProfile = () => {
 	const { user: userData, error: userDataError } = useSelector(
 		(state) => state.authReducer
 	)
-	const [watchListLecs, setWatchListLecs] = useState([])
+	const [watchListLecs, setWatchListLecs] = useState(watchListLecsSample)
 	const [watchListCourses, setWatchListCourses] = useState([])
 	const [watchListUnis, setWatchListUnis] = useState([])
 	const [selectedUni, setSelectedUni] = useState(null)
 	const [showUniModal, setShowUniModal] = useState(false)
+	const [profileMenuOpen, setProfileMenuOpen] = useState(false)
 
 	const dispatch = useDispatch()
 
@@ -52,11 +89,13 @@ const StudentProfile = () => {
 			dispatch(register(newUserData))
 		}
 	}
+
 	useEffect(() => {
 		if (userData && userData.university) {
 			setShowUniModal(false)
 		}
 	}, [userData])
+
 	const handleUniChange = (selectedOption) => {
 		setSelectedUni(selectedOption)
 	}
@@ -67,74 +106,147 @@ const StudentProfile = () => {
 	}))
 
 	return (
-		<div className='container mx-auto px-4'>
-			<SignedIn>
-				<div className='flex justify-between items-center my-4'>
-					<h1 className='text-2xl font-bold'>Student Profile</h1>
-					<UserButton />
-				</div>
-				{showUniModal && (
-					<div className='fixed z-50 inset-0 overflow-y-auto'>
-						<div className='flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
-							<div className='fixed inset-0 transition-opacity'>
-								<div className='absolute inset-0 bg-gray-500 opacity-75'></div>
+		<div className='bg-gray-100 min-h-screen'>
+			{/* Main content section */}
+			<div className='max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8'>
+				<div className='max-w-3xl mx-auto'>
+					<h1 className='text-2xl font-semibold text-gray-800 mb-4'>
+						My Profile
+					</h1>
+					{uniError && (
+						<div className='bg-red-500 text-white py-2 px-4 rounded-md mb-4'>
+							{uniError}
+						</div>
+					)}
+					{userDataError && (
+						<div className='bg-red-500 text-white py-2 px-4 rounded-md mb-4'>
+							{userDataError}
+						</div>
+					)}
+
+					{/* University selection modal */}
+					{showUniModal && (
+						<div className='bg-white rounded-md border-2 border-gray-200 py-4 px-4 mb-4'>
+							<h2 className='text-lg font-semibold mb-2'>
+								Select Your University
+							</h2>
+							<Select
+								options={universityOptions}
+								onChange={handleUniChange}
+								placeholder='Select university'
+							/>
+							<button
+								className='bg-blue-500 text-white px-4 py-2 rounded-md mt-4'
+								onClick={handleSelectUni}>
+								Save
+							</button>
+						</div>
+					)}
+
+					{/* University info */}
+					{uni && (
+						<div className='bg-white rounded-md border-2 border-gray-200 py-4 px-4 mb-4'>
+							<h2 className='text-lg font-semibold mb-2'>{uni.name}</h2>
+
+							<div className='text-gray-800 mb-2'>Location: {uni.location}</div>
+						</div>
+					)}
+
+					<div className='bg-white rounded-md border-2 border-gray-200 py-4 px-4 mb-4'>
+						<h2 className='text-lg font-semibold mb-2'>My Watchlist</h2>
+
+						{/* Watchlist - Lecturers */}
+						<div className='mb-4'>
+							<div className='flex items-center justify-between mb-2'>
+								<h3 className='text-md font-semibold text-teal-500'>
+									Lecturers
+								</h3>
+								<div className='text-teal-500 cursor-pointer'>
+									<FaPlusSquare />
+								</div>
 							</div>
-							<span className='hidden sm:inline-block sm:align-middle sm:h-screen'></span>
-							&#8203;
-							<div
-								className='inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'
-								role='dialog'
-								aria-modal='true'
-								aria-labelledby='modal-headline'>
-								<div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
-									<div className='sm:flex sm:items-start'>
-										<div className='mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10'>
-											<svg
-												className='h-6 w-6 text-blue-600'
-												xmlns='http://www.w3.org/2000/svg'
-												fill='none'
-												viewBox='0 0 24 24'
-												stroke='currentColor'
-												aria-hidden='true'>
-												<path
-													strokeLinecap='round'
-													strokeLinejoin='round'
-													strokeWidth={2}
-													d='M16 17l-4 4m0 0l-4-4m4 4V3'
-												/>
-											</svg>
-										</div>
-										<div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
-											<h3
-												className='text-lg leading-6 font-medium text-gray-900'
-												id='modal-headline'>
-												Select Your University
-											</h3>
-											<div className='mt-2'>
-												<Select
-													options={universityOptions}
-													onChange={handleUniChange}
-												/>
+
+							{watchListLecs.length > 0 ? (
+								<div className='grid gap-4'>
+									{watchListLecs.map((lecturer) => (
+										<div
+											key={lecturer._id}
+											className='bg-teal-50 rounded-md p-4 flex items-center justify-between'>
+											<div className='flex items-center'>
+												<div className='text-teal-500 mr-2'>
+													<FaUserTie />
+												</div>
+												<div>
+													<div className='font-medium'>{lecturer.name}</div>
+													<div className='text-gray-600'>{lecturer.email}</div>
+													<div className='text-gray-600'>
+														{lecturer.university.code}
+													</div>
+												</div>
 											</div>
-											{uniError && (
-												<div className='text-red-500 mt-2'>{uniError}</div>
-											)}
+											<div className='flex items-center'>
+												<div className='text-teal-500 mr-2'>
+													<FaStar />
+												</div>
+												<div className='text-gray-600'>
+													{lecturer?.avgRating ?? 4.5}
+												</div>
+											</div>
 										</div>
-									</div>
+									))}
 								</div>
-								<div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
-									<button
-										type='button'
-										onClick={handleSelectUni}
-										className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm'>
-										Save
-									</button>
+							) : (
+								<div className='text-gray-600'>No lecturers found.</div>
+							)}
+						</div>
+
+						{/* Watchlist - Courses */}
+						<div>
+							<div className='flex items-center justify-between mb-2'>
+								<h3 className='text-md font-semibold text-teal-500'>Courses</h3>
+								<div className='text-teal-500 cursor-pointer'>
+									<FaPlusSquare />
 								</div>
 							</div>
+
+							{watchListCourses.length > 0 ? (
+								<div className='grid gap-4'>
+									{watchListCourses.map((course) => (
+										<div
+											key={course._id}
+											className='bg-teal-50 rounded-md p-4 flex items-center justify-between'>
+											<div className='flex items-center'>
+												<div className='text-teal-500 mr-2'>
+													<FaBook />
+												</div>
+												<div>
+													<div className='font-medium'>{course.name}</div>
+													<div className='text-gray-600'>{course.code}</div>
+													<div className='text-gray-600'>
+														{course.department}
+													</div>
+												</div>
+											</div>
+											<div className='flex items-center'>
+												<div className='text-teal-500 mr-2'>
+													<FaStar />
+												</div>
+												<div className='text-gray-600'>
+													{course?.avgRating ?? 4.5}
+												</div>
+											</div>
+										</div>
+									))}
+								</div>
+							) : (
+								<div className='text-gray-600'>No courses found.</div>
+							)}
 						</div>
 					</div>
-				)}
-			</SignedIn>
+
+					{/* Recommended section */}
+				</div>
+			</div>
 		</div>
 	)
 }
