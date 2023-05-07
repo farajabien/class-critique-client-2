5b87dfcc27a6533c327663a80ec71f75339913dc
Bienvenu <faraja.bien@gmail.com>
2023-05-07 14:07:16 +0300

    cors fix https

--- components/universities/AddUniversityModal.jsx
+++ components/universities/AddUniversityModal.jsx
@@ -36,7 +36,7 @@ export default function AddUniversityModal({
 		const fetchData = async () => {
 			try {
 				const response = await axios.get(
-					'http://universities.hipolabs.com/search?country=kenya'
+					'https://universities.hipolabs.com/search?country=kenya'
 				)
 				const data = response.data
 					.filter((university, index, self) => {
